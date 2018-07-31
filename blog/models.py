e041702f38e701a96274f7f83b67430532fdd0ed
Goran <goranmrd@gmail.com>
2018-07-31 15:31:52 +0200

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,27 @@
-from django.db import models 
+from django.db import models
 from django.utils import timezone
 from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from notifications.signals import notify
 
-# Create your models here. 
-class Post(models.Model): 
-    author = models.ForeignKey('auth.User', on_delete=models.CASCADE) 
-    title = models.CharField(max_length=200) 
-    text = models.TextField() 
-    created_date = models.DateTimeField(default=timezone.now) 
-    published_date = models.DateTimeField(blank=True, null=True) 
 
-    def publish(self): 
-        self.published_date = timezone.now() 
-        self.save() 
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-    def __str__(self): 
-        return self.title 
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approved_comments(self):
+        return self.comments.filter(approved_comment=True)
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
@@ -30,9 +34,6 @@ class Comment(models.Model):
     def approve(self):
         self.approved_comment = True
         self.save()
-
-    def approved_comments(self):
-        return self.comments.filter(approved_comment=True)
 
     def __str__(self):
         return self.text
